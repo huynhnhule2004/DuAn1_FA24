df8066d64e20a1a9c7bbf93a033686e4325f0f89
huynhnhule2004 <huynhnhule2004@gmail.com>
2024-11-08 09:50:13 +0700

    [Client] Bổ sung hồ sơ

--- App/Controllers/Client/ProfileController.php
+++ App/Controllers/Client/ProfileController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controllers\Client;
 
-
-use App\Helpers\NotificationHelper;
-use App\Views\Client\Components\Notification;
 use App\Views\Client\Pages\Profile\Profile;
 use App\Views\Client\Layouts\Header;
 use App\Views\Client\Layouts\Footer;
 
 class ProfileController
 {
-    // hiển thị danh sách
+    // Hiển thị trang hồ sơ người dùng
     public static function index()
     {
+        
+        $data = [
+            'first_name' => 'Ca Moi',
+            'last_name' => '3 Co Gai',
+            'email' => 'hoso@gmail.com',
+            'avatar' => '/public/assets/client/images/staff-1.jpg',  // Đường dẫn ảnh đại diện
+            'phone' => '(333) 000 555',
+            'address' => 'HCM',
+            'social' => 'camoi3cogai'
+        ];
+
+        // Truyền dữ liệu vào view
         Header::render();
-        Profile::render();
+        Profile::render($data);
         Footer::render();
     }
 }
+
