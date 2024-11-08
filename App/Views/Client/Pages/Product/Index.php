7bd0ed49be9a7eef67cbfc69fa0546deccaf9ff7
Lypham <camlypham61@gmail.com>
2024-11-08 23:44:11 +0700

    [Client] Chỉnh sửa index/css

--- App/Views/Client/Pages/Product/Index.php
+++ App/Views/Client/Pages/Product/Index.php
@@ -42,15 +42,11 @@ class Index extends BaseView
 
           <div class="d-flex justify-content-between align-items-center mb-3">
 
-            <h1 class="mb-0">Sản phẩm</h1>
+            <h2 class="mb-0">Sản phẩm</h2>
             <select class="filter-categories border-0 m-0">
                 <option value="">Sắp xếp mặc định</option>
                 <option value="">Tên (A - Z)</option>
                 <option value="">Tên (Z - A)</option>
-                <option value="">Giá (Thấp-Cao)</option>
-                <option value="">Giá (Cao-Thấp)</option>
-                <option value="">Đánh giá (Cao)</option>
-                <option value="">Đánh giá (Thấp)</option>
               </select>
           </div>
 
@@ -68,7 +64,7 @@ class Index extends BaseView
                       </a>
                       <div class="card-body p-0">
                         <a href="/products/<?= $item['id'] ?>">
-                          <h3 class="card-title pt-4 m-0"><?= $item['name'] ?></h3>
+                          <h4 class="card-title pt-4 m-0"><?= $item['name'] ?></h4>
                         </a>
                         <div class="card-text">
                           <span class="rating secondary-font">
@@ -79,7 +75,7 @@ class Index extends BaseView
                             <iconify-icon icon="clarity:star-solid" class="text-primary"></iconify-icon>
                             5.0
                           </span>
-                          <h3 class="secondary-font text-primary"><?= $item['price'] ?></h3>
+                          <h4 class="secondary-font text-primary"><?= $item['price'] ?></h4>
 
                           <div class="d-flex flex-wrap mt-3">
                             <a href="#" class="btn-cart me-3 px-4 pt-3 pb-3">
@@ -112,17 +108,17 @@ class Index extends BaseView
       <div class="container my-5 ">
         <div class="row my-5 py-5">
           <div class="offset-md-3 col-md-6 my-5 ">
-            <h2 class="display-3 fw-normal text-center">Giảm giá 20%</h2>
-            <h2 class="display-3 fw-normal text-center"><span class="text-primary">Khi mua lần đầu</span></h2>
+            <h2 class="display-4 fw-normal text-center">Giảm giá 20%</h2>
+            <h2 class="display-4 fw-normal text-center"><span class="text-primary">Khi mua lần đầu</span></h2>
             <form>
               <div class="mb-3">
-                <input type="email" class="form-control form-control-lg" name="email" id="email" placeholder="Nhập Email của bạn">
+                <input type="email" class="form-control form-control-sm" name="email" id="email" placeholder="Nhập Email của bạn">
               </div>
               <div class="mb-3">
-                <input type="password" class="form-control form-control-lg" name="email" id="password1" placeholder="Mật khẩu">
+                <input type="password" class="form-control form-control-sm" name="email" id="password1" placeholder="Mật khẩu">
               </div>
               <div class="mb-3">
-                <input type="password" class="form-control form-control-lg" name="email" id="password2" placeholder="Nhập lại mật khẩu">
+                <input type="password" class="form-control form-control-sm" name="email" id="password2" placeholder="Nhập lại mật khẩu">
               </div>
 
               <div class="d-grid gap-2">
@@ -133,7 +129,7 @@ class Index extends BaseView
         </div>
       </div>
     </section>
-    <section id="service" class="mt-5 pt-2">
+    <section id="service">
       <div class="container py-5 my-5">
         <div class="row g-md-5 pt-4">
           <div class="col-md-3 my-3">
@@ -141,9 +137,9 @@ class Index extends BaseView
               <div>
                 <iconify-icon class="service-icon text-primary" icon="la:shopping-cart"></iconify-icon>
               </div>
-              <h3 class="card-title py-2 m-0">Miễn phí vận chuyển</h3>
+              <h3 class="card-title py-2 m-0">Giao Hàng Miễn Phí</h3>
               <div class="card-text">
-                <p class="blog-paragraph fs-6">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
+                <p class="blog-paragraph fs-6">Chúng tôi hỗ trợ giao hàng miễn phí cho đơn hàng >200</p>
               </div>
             </div>
           </div>
@@ -152,9 +148,9 @@ class Index extends BaseView
               <div>
                 <iconify-icon class="service-icon text-primary" icon="la:user-check"></iconify-icon>
               </div>
-              <h3 class="card-title py-2 m-0">Thanh toán an toàn 100%</h3>
+              <h3 class="card-title py-2 m-0">Thanh Toán Nhanh</h3>
               <div class="card-text">
-                <p class="blog-paragraph fs-6">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
+                <p class="blog-paragraph fs-6">Đảm bảo thanh toán an toàn, bảo mật tuyệt đối cho khách hàng.</p>
               </div>
             </div>
           </div>
@@ -163,9 +159,9 @@ class Index extends BaseView
               <div>
                 <iconify-icon class="service-icon text-primary" icon="la:tag"></iconify-icon>
               </div>
-              <h3 class="card-title py-2 m-0">Ưu đãi hàng ngày</h3>
+              <h3 class="card-title py-2 m-0">Ưu Đãi Hàng Ngày</h3>
               <div class="card-text">
-                <p class="blog-paragraph fs-6">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
+                <p class="blog-paragraph fs-6">Nhận ngay các ưu đãi hấp dẫn mỗi ngày khi mua sắm tại cửa hàng.</p>
               </div>
             </div>
           </div>
@@ -174,9 +170,9 @@ class Index extends BaseView
               <div>
                 <iconify-icon class="service-icon text-primary" icon="la:award"></iconify-icon>
               </div>
-              <h3 class="card-title py-2 m-0">Đảm bảo chất lượng</h3>
+              <h3 class="card-title py-2 m-0">Đảm Bảo Yêu Cầu</h3>
               <div class="card-text">
-                <p class="blog-paragraph fs-6">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
+                <p class="blog-paragraph fs-6">Cam kết mang đến sản phẩm chất lượng tốt nhất cho thú cưng của bạn.</p>
               </div>
             </div>
           </div>
