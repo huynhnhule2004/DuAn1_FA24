9a4673b60d455bcbfacc7faddcdf0c5a0faa36c9
huynhnhule2004 <huynhnhule2004@gmail.com>
2024-11-08 09:49:52 +0700

    [Client] Bổ sung giỏ hàng

--- App/Controllers/Client/CartController.php
+++ App/Controllers/Client/CartController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controllers\Client;
 
-
 use App\Helpers\NotificationHelper;
 use App\Views\Client\Components\Notification;
 use App\Views\Client\Pages\Cart\Cart;
@@ -14,8 +13,36 @@ class CartController
     // hiển thị danh sách
     public static function index()
     {
+        // Dữ liệu ví dụ
+        $data = [
+            [
+                'name' => 'Mimo thức ăn mèo',
+                'image' => '/public/assets/client/images/22.jpg',
+                'size' => '5kg',
+                'format' => 'Túi',
+                'quantity' => 2,
+                'price' => 9.99
+            ],
+            [
+                'name' => 'Whiskas thức ăn mèo',
+                'image' => '/public/assets/client/images/123.jpg',
+                'size' => '10kg',
+                'format' => 'Túi',
+                'quantity' => 1,
+                'price' => 13.50
+            ],
+            [
+                'name' => 'Ganador thức ăn chó',
+                'image' => '/public/assets/client/images/gana.jpg',
+                'size' => '10kg',
+                'format' => 'Túi',
+                'quantity' => 1,
+                'price' => 13.50
+            ]
+        ];
+
         Header::render();
-        Cart::render();
+        Cart::render($data); // Truyền dữ liệu vào view
         Footer::render();
     }
 }
