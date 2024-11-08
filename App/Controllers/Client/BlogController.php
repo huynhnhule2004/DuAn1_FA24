18ded9af27f79ec84976ea02b0ff5944f17055af
huynhnhule2004 <huynhnhule2004@gmail.com>
2024-11-08 09:45:44 +0700

    [Client] Bổ sung trang Blog

--- App/Controllers/Client/BlogController.php
+++ App/Controllers/Client/BlogController.php
@@ -13,8 +13,31 @@ class BlogController
     // hiển thị danh sách
     public static function index()
     {
+        $blogs = [
+            [
+            'image' => 'blog1.jpg',
+            'title' => '10 lý do để giúp đỡ bất kỳ loài động vật nào',
+            'content' => 'Cốt lõi trong hoạt động của chúng tôi là ý tưởng rằng các thành phố là vườn ươm của
+                        những thành tựu lớn nhất của chúng ta và niềm hy vọng tốt đẹp nhất cho một tương lai bền vững.'
+            ],
+            [
+                'image' => 'blog2.jpg',
+                'title' => 'Cách để nhận biết thú cưng của bạn đang đói',
+                'content' => 'Cốt lõi trong hoạt động của chúng tôi là ý tưởng rằng các thành phố là vườn ươm của
+                            những thành tựu lớn nhất của chúng ta và niềm hy vọng tốt đẹp nhất cho một tương lai bền vững.'
+                ],
+                [
+                    'image' => 'blog3.jpg',
+                    'title' => 'Ngôi Nhà Tốt Nhất Cho Thú Cưng Của Bạn',
+                    'content' => 'Cốt lõi trong hoạt động của chúng tôi là ý tưởng rằng các thành phố là vườn ươm của
+                                những thành tựu lớn nhất của chúng ta và niềm hy vọng tốt đẹp nhất cho một tương lai bền vững.'
+                    ],
+        ];
+        $data = [
+            'blogs' => $blogs,
+        ];
         Header::render();
-        Index::render();
+        Index::render($data);
         Footer::render();
     }
 }
